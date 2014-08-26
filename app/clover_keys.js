e5dae03e783dfc0858306a3f69a32cc856e1b4af
George Ashworth <georgeliuashworth@gmail.com>
2014-08-26 01:19:07 -0700

    updated test api token

--- app/clover_keys.js
+++ app/clover_keys.js
@@ -1,6 +1,6 @@
 (function(){
   BarberApp.baseURL =  'https://api.clover.com/v3/';
-  BarberApp.accessToken = '26686e97-c1a7-9294-cf8b-8c63ac66b393';
+  BarberApp.accessToken = '7bd9e627-cd42-3ae4-6b1d-417e9d33eb60';
 	BarberApp.merchantID = 'V6ATA6FESWVXW';
 }();
   
